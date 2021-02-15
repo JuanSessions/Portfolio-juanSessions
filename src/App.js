605ca9d18a24d09aa727696f2c52133ccffda710
JuanSessions <juanperusin@yahoo.es>
2021-02-15 13:39:23 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './style/App.css';
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import Routes from './Routes';
 
