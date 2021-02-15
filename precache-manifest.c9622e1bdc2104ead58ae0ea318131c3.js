f58dfff74a07c04b67fc388c1fa77cf58c0f7bcd
JuanSessions <juanperusin@yahoo.es>
2021-02-15 11:03:39 +0100

    Updates

---
+++ precache-manifest.c9622e1bdc2104ead58ae0ea318131c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962cea9ef7ffe8d09726db56bfc40149",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ad1987fd83384bb770e",
+    "url": "./static/css/main.d39f1d1a.chunk.css"
+  },
+  {
+    "revision": "97e0e3aaac3749fb8abe",
+    "url": "./static/js/2.d74fd371.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.d74fd371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4983825925cccb242b69",
+    "url": "./static/js/3.d81de6c1.chunk.js"
+  },
+  {
+    "revision": "196f31064c641ef86b22f34244b7ad03",
+    "url": "./static/js/3.d81de6c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad1987fd83384bb770e",
+    "url": "./static/js/main.88a36dde.chunk.js"
+  },
+  {
+    "revision": "aa18213e3589cbd73e71",
+    "url": "./static/js/runtime-main.c53b2e64.js"
+  },
+  {
+    "revision": "e91b1f21391b7be5e2209fd084db67bd",
+    "url": "./static/media/faxsound.e91b1f21.wav"
+  },
+  {
+    "revision": "b8cdd0ec340e161bb3fb5cc8daa0b0d1",
+    "url": "./static/media/flyby.b8cdd0ec.mp3"
+  },
+  {
+    "revision": "e2d4f4378553b059402b6036bb190359",
+    "url": "./static/media/mic.e2d4f437.jpg"
+  },
+  {
+    "revision": "19a85d35e9724a47fea05aa2ebe1cd9e",
+    "url": "./static/media/navbarsound.19a85d35.wav"
+  },
+  {
+    "revision": "fe7a1614ff181af8ee44f81a52d41815",
+    "url": "./static/media/portfolio-mask.fe7a1614.jpg"
+  }
+]);
