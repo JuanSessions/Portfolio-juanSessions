f114e25215c669b52c16054a93cd4ea13c7b6e5d
JuanSessions <juanperusin@yahoo.es>
2021-03-03 15:55:02 +0100

    new

--- src/components/CardStatic.jsx
+++ src/components/CardStatic.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-// import videoCassette from '../videos/cassette.mp4'
-// import videoTv from '../videos/video-tv.mp4'
 
 export default function CardStatic() {
     return (
         <>
             <li className='cards__item card-two'>
-                <a href='https://www.youtube.com/watch?v=cX1PNMyGPhs&t=7s' className='cards__item__link' target="blank">
+                <a href='https://www.youtube.com/watch?v=zaiS9qKxfD4' className='cards__item__link' target="blank">
 
                     <div className='cards__item__pic-wrap' id="card-video-container" data-category="Videos">
                         <h3 id="card-video-title">juanSessions</h3>
